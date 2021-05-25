f54a51fc70eb7cadb9d8d8304cf9aea100f4138b
Ahmet Özberk <ahmet.ozberk.mrs@gmail.com>
2021-05-25 15:42:44 +0300

    HRMS Project
    Java Camp HRMS Project

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
@@ -0,0 +1,38 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import kodlamaio.hrms.business.abstracts.JobPositionService;
+import kodlamaio.hrms.core.utilities.result.DataResult;
+import kodlamaio.hrms.core.utilities.result.Result;
+import kodlamaio.hrms.core.utilities.result.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.result.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.JobPositionDao;
+import kodlamaio.hrms.entities.concretes.JobPosition;
+
+@Service
+public class JobPositionManager implements JobPositionService{
+	private JobPositionDao jobPositionDao;
+	
+	@Autowired
+	public JobPositionManager(JobPositionDao jobPositionDao) {
+		super();
+		this.jobPositionDao = jobPositionDao;
+	}
+
+	@Override
+	public DataResult<List<JobPosition>> getAll() {
+		return new SuccessDataResult<List<JobPosition>>(this.jobPositionDao.findAll(),"Veri listelendi");		
+	}
+
+	@Override
+	public Result add(JobPosition jobPosition) {
+		this.jobPositionDao.save(jobPosition);
+		return new SuccessResult("Eklendi.");
+	}
+
+}
